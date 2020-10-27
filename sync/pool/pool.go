ad375526e3c5af9d4aa7e7db7375efafc0248a2b
Salif Dianda <37021551+AutismPatient@users.noreply.github.com>
2020-10-27 22:08:52 +0800

    pool 待完善

---
+++ sync/pool/pool.go
@@ -0,0 +1,75 @@
+package pool
+
+import (
+	"runtime"
+	"sync"
+)
+
+/*
+
+	基于协程实现的协程池 2020年10月27日20:08:29
+ 	Goroutine Pool
+
+*/
+
+// 池接受来自客户端的任务，它限制总数
+// 通过回收goroutines到一个给定的数字的goroutines。
+type Pool struct {
+	// 容量
+	capacity int
+
+	actionJob chan *Task
+
+	transmitChan chan *Task
+
+	// 同步操作的锁
+	lock sync.Mutex
+
+	once sync.Once
+}
+
+type Task struct {
+	fc func() error
+}
+
+func NewPool(cap int) *Pool {
+	if cap >= runtime.NumCPU() {
+		cap = runtime.NumCPU()
+	} else if cap <= 0 {
+		cap = 1
+	}
+	return &Pool{
+		capacity:     cap,
+		actionJob:    make(chan *Task),
+		transmitChan: make(chan *Task),
+		lock:         sync.Mutex{},
+		once:         sync.Once{},
+	}
+}
+
+func NewTask(f func() error) *Task {
+	return &Task{fc: f}
+}
+
+func (t *Task) execute() {
+	_ = t.fc()
+}
+
+func (p *Pool) work() {
+	for aj := range p.actionJob {
+		go aj.execute()
+	}
+}
+
+func (p *Pool) goRoutines(t *Task) {
+	go func() {
+		p.actionJob <- t
+	}()
+}
+
+func (p *Pool) Run() {
+
+	for i := 0; i <= p.capacity; i++ {
+		go p.work()
+	}
+}
---
+++ sync/pool/pool_test.go
@@ -0,0 +1,31 @@
+package pool
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+/*
+ testing
+*/
+
+func TestRunPool(t *testing.T) {
+
+	p := NewPool(2)
+
+	f := func() error {
+		for i := 1; i <= 100; i++ {
+			fmt.Print(i)
+		}
+		return nil
+	}
+
+	task := NewTask(f)
+
+	p.goRoutines(task)
+
+	p.Run()
+
+	time.Sleep(time.Second * 2)
+}

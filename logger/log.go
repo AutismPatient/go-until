1fcf17bad765f855383340c17acf78184cb67f95
Salif Dianda <37021551+AutismPatient@users.noreply.github.com>
2021-02-01 22:36:46 +0800

    logger

--- logger/log.go
+++ logger/log.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/sirupsen/logrus"
+	"io"
 )
 
 var (
@@ -18,15 +19,18 @@ func init() {
 参数
 */
 type Parameter struct {
-	Formatter    LogoutsFormatter // 自定义格式化
-	Level        logrus.Level     // 级别
-	ReportCaller bool             // 如果您希望将调用方法添加为字段
-	Fields       logrus.Fields    // 自定义字段map
+	Formatter           LogoutsFormatter // 自定义格式化
+	Level               logrus.Level     // 级别
+	ReportCaller        bool             // 如果您希望将调用方法添加为字段
+	Fields              logrus.Fields    // 自定义字段map
+	Hook                logrus.Hook      // 您可以添加用于日志记录级别的挂钩。例如，将错误发送到上的异常跟踪服务Error，Fatal并将Panic信息发送到StatsD或同时记录到多个位置，例如syslog。
+	IO                  io.Writer        // io
+	RegisterExitHandler func()           // 程序异常后即将退出事件
+	DeferExitHandler    func()           //
 }
 
 /**
-go日志封装
-来自：log
+返回 logrus 实例
 */
 func newLog(parameter Parameter) *logrus.Logger {
 	DefaultLogouts = logrus.New()
@@ -34,6 +38,12 @@ func newLog(parameter Parameter) *logrus.Logger {
 	DefaultLogouts.SetLevel(parameter.Level)
 	DefaultLogouts.SetReportCaller(parameter.ReportCaller)
 	DefaultLogouts.WithFields(parameter.Fields)
+	DefaultLogouts.AddHook(parameter.Hook)
+	DefaultLogouts.SetOutput(parameter.IO)
+
+	logrus.RegisterExitHandler(parameter.RegisterExitHandler)
+	logrus.DeferExitHandler(parameter.DeferExitHandler)
+
 	return DefaultLogouts
 }
 
